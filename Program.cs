b2bb77937c6ebeb23c36bee85700e85aadb04163
natanaeladit <nata.adit@gmail.com>
2020-04-19 20:34:52 +0800

    aws cognito authentication

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Amazon.Runtime;
+using Amazon.CognitoIdentityProvider;
+using Amazon.Extensions.CognitoAuthentication;
+using System.Threading.Tasks;
+using Amazon;
+
+namespace aws_cognito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            string username = "username";
+            string password = "password";
+            string clientId = "clientId";
+            string poolId = "poolId";
+            Console.WriteLine(await AuthenticateWithSrpAsync(username, password, poolId, clientId));
+            Console.ReadLine();
+        }
+
+        public static async Task<string> AuthenticateWithSrpAsync(string username, string password, string poolId, string clientId)
+        {
+            AmazonCognitoIdentityProviderClient provider = new AmazonCognitoIdentityProviderClient(new AnonymousAWSCredentials(), RegionEndpoint.APSoutheast1);
+
+            CognitoUserPool userPool = new CognitoUserPool(poolId, clientId, provider);
+            CognitoUser user = new CognitoUser(username, clientId, userPool, provider);
+
+            AuthFlowResponse context = await user.StartWithSrpAuthAsync(new InitiateSrpAuthRequest
+            {
+                Password = password
+            }).ConfigureAwait(false);
+
+            string token = context?.AuthenticationResult?.AccessToken;
+            return token;
+        }
+    }
+}
